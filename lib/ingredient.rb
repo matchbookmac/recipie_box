e714c556a41385434a1a98b7b625f41b1ee4ff13
Alec Arme & Ian MacDonald <student@epicodus.com>
2015-05-14 16:25:24 -0700

    Add validtation for ingredient name uniqueness. Passes spec.

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,3 +1,4 @@
 class Ingredient < ActiveRecord::Base
-has_and_belongs_to_many :recipes
+  has_and_belongs_to_many :recipes
+  validates :name, {presence: true, uniqueness: { case_sensitive: false}}
 end
